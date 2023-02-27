a5ed4e60c9c0246dd1994b5d850e672917ff0e76
HartJN <jameshart04@gmail.com>
2023-02-27 17:16:53 +1300

    feat: finds user by email&pass

--- server/src/modules/user/user.service.ts
+++ server/src/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import argon2 from 'argon2';
 import crypto from 'crypto';
 import { UserModel } from './user.model';
 
@@ -13,4 +14,26 @@ export async function createUser(input: {
     email: input.email,
     password: input.hashedPassword,
   });
+}
+
+async function generateHash(password: string) {
+  return argon2.hash(password);
+}
+
+export async function findUserByEmailAndPassword({
+  email,
+  hashedPassword,
+}: {
+  email: string;
+  hashedPassword: string;
+}) {
+  const user = await UserModel.findOne({ email });
+
+  const hash = await generateHash(hashedPassword);
+
+  if (!user || !argon2.verify(user.password, hash)) {
+    return null;
+  }
+
+  return user;
 }
