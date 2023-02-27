6dd18324ae54fda78f82f2112b56b617db51bb5d
HartJN <jameshart04@gmail.com>
2023-02-27 16:20:08 +1300

    feat: updates vault

--- server/src/modules/vault/vault.service.ts
+++ server/src/modules/vault/vault.service.ts
@@ -3,3 +3,13 @@ import { VaultModel } from './vault.model';
 export function createVault(input: { user: string; salt: string }) {
   return VaultModel.create(input);
 }
+
+export function updateVault({
+  userId,
+  data,
+}: {
+  userId: string;
+  data: string;
+}) {
+  return VaultModel.updateOne({ user: userId }, { data });
+}
