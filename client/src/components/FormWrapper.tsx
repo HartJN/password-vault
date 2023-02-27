744a2e1c3a186fe87636461c827d70629a10941f
HartJN <jameshart04@gmail.com>
2023-02-27 15:49:50 +1300

    feat: adds form wrapper

--- client/src/components/FormWrapper.tsx
+++ client/src/components/FormWrapper.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 function FormWrapper({
   children,
   ...props
-}: { child: React.ReactNode } & BoxProps) {
+}: { children: React.ReactNode } & BoxProps) {
   return (
     <Box w="100%" maxW="container.sm" boxShadow="xl" p="8" as="form" {...props}>
       {children}
