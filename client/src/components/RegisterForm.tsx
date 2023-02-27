87ec41fd879e0090f4745255ed5bda4da25faa57
HartJN <jameshart04@gmail.com>
2023-02-27 16:17:57 +1300

    feat: adds step and vault state

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -1,14 +1,26 @@
+import { registerUser } from '@/api';
+import { generateVaultKey, hashPassword } from '@/crypto';
+import { VaultItem } from '@/pages';
 import {
+  Button,
   FormControl,
   FormErrorMessage,
   FormLabel,
   Heading,
   Input,
 } from '@chakra-ui/react';
+import { useMutation } from '@tanstack/react-query';
+import { Dispatch, SetStateAction } from 'react';
 import { useForm } from 'react-hook-form';
 import FormWrapper from './FormWrapper';
 
-function RegisterForm() {
+function RegisterForm({
+  setVaultKey,
+  setStep,
+}: {
+  setVaultKey: Dispatch<SetStateAction<string>>;
+  setStep: Dispatch<SetStateAction<'login' | 'register' | 'vault'>>;
+}) {
   const {
     handleSubmit,
     register,
@@ -17,11 +29,38 @@ function RegisterForm() {
     formState: { errors, isSubmitting },
   } = useForm<{ email: string; password: string; hashedPassword: string }>();
 
+  const mutation = useMutation(registerUser, {
+    onSuccess: ({ salt, vault }) => {
+      const hashedPassword = getValues('hashedPassword');
+
+      const email = getValues('email');
+
+      const vaultKey = generateVaultKey({ hashedPassword, email, salt });
+
+      window.sessionStorage.setItem('vk', vaultKey);
+
+      setVaultKey(vaultKey);
+
+      window.sessionStorage.setItem('vault', '');
+
+      setStep('vault');
+    },
+  });
+
   return (
     <FormWrapper
       onSubmit={handleSubmit(() => {
         const email = getValues('email');
         const password = getValues('password');
+
+        const hashedPassword = hashPassword(password);
+
+        setValue('hashedPassword', hashedPassword);
+
+        mutation.mutate({
+          email,
+          hashedPassword,
+        });
       })}
     >
       <Heading>Register</Heading>
@@ -60,6 +99,8 @@ function RegisterForm() {
           {errors.password && errors.password.message}
         </FormErrorMessage>
       </FormControl>
+
+      <Button type="submit">{isSubmitting ? 'Loading...' : 'Register'}</Button>
     </FormWrapper>
   );
 }
