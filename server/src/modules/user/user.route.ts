7c3b99e265d9e9eb1f37293becc7b763b04e282b
HartJN <jameshart04@gmail.com>
2023-02-27 13:54:55 +1300

    feat: adds user module

---
+++ server/src/modules/user/user.route.ts
@@ -0,0 +1,11 @@
+import { FastifyInstance, FastifyPluginOptions, FastifyError } from 'fastify';
+
+function userRoutes(
+  app: FastifyInstance,
+  _: FastifyPluginOptions,
+  done: (err?: FastifyError) => void
+) {
+  done();
+}
+
+export default userRoutes;
